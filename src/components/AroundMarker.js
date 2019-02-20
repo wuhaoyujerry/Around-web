cf35058cc9908c6458be58b87efbefdf032bed23
Jerry Wu <wuhaoyu@bu.edu>
2019-02-20 18:21:23 -0500

    Continue Google Map Implementation
    Signed-off-by: Jerry Wu <wuhaoyu@bu.edu>

---
+++ src/components/AroundMarker.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {InfoWindow, Marker} from "react-google-maps";
+
+class AroundMarker extends React.Component {
+	state = {
+		isOpen: false,
+	}
+
+	onToggleOpen = () => {
+		this.setState((prevState) => {
+			return {isOpen: !prevState.isOpen}
+		});
+	}
+
+	render() {
+		const {location, url, message, user} = this.props.post;
+		return (
+			<Marker
+				position={{lat: location.lat, lng: location.lon}}
+				onMouseOver={this.onToggleOpen}
+				onMouseOut={this.onToggleOpen}
+			>
+				{this.state.isOpen ?
+					<InfoWindow onCloseClick={this.onToggleOpen}>
+						<div>
+							<img className="around-marker-image" src={url} alt={`${user}:${message}`}/>
+							<p>{`${user}: ${message}`}</p>
+						</div>
+					</InfoWindow>
+					: null}
+			</Marker>
+		);
+	}
+}
+
+export {AroundMarker}
